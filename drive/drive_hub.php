ae4ee4e64044f2f0b74282b1b4094410e69c24c2
Royo Arnaud <arnaud.royo@epsi.fr>
2019-09-10 09:20:35 +0200

    index

--- drive/drive_hub.php
+++ drive/drive_hub.php
@@ -14,7 +14,7 @@
 <p><a href="addproject.php">Créer projets<a></p>
 <p>ici: mes projets</p>
 <?php
-    
+$_SESSION['id']=1;
 // On récupère le titre des projets dont l'users est membre 
 $reponse = $bdd->query('SELECT titre FROM projets WHERE id_membres ='.$_SESSION['id'].'');
 // On affiche chaque entrée une à une

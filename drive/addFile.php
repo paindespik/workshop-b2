fd16be54bfa83bd6f7351f9b4b6ea9d56938d665
paindespik <seandriard3@gmail.com>
2019-09-10 12:08:17 +0200

    new commit

--- drive/addFile.php
+++ drive/addFile.php
@@ -1,5 +1,6 @@
 <?php
 
+	$resultat=1;
 	include("../getbdd.php");
 	if ($_FILES['nomFichier']['error'] > 0) {
 		$resultat = -1;
@@ -9,5 +10,48 @@
 	}
 
 	$extensions_valides = array('jpg', 'jpeg', 'png', 'rar', 'zip', 'docx', 'txt', 'html', 'php');
-	
+	$extensionsUploaded = strtolower(substr(strrchr($_FILES['nomFichier']['name'],'.') , 1) );
+	if (! in_array($extensionsUploaded,$extensions_valides)){
+		$resultat = -3;
+	}
+
+	if ($resultat == 1){
+		$fileName="f".$id_projet.$newId.".".$extensionsUploaded;
+		$fullPathName = DIRECTORY_SEPARATOR."fichiers".DIRECTORY_SEPARATOR.$fileName;
+		$serverDirectory=__DIR__;
+		$index =strrpos($serverDirectory, DIRECTORY_SEPARATOR);
+		$serverDirectory = substr($serverDirectory, DIRECTORY_SEPARATOR);
+		$serverDirectory = DIRECTORY_SEPARATOR;
+		$fullPathName = DIRECTORY_SEPARATOR."fichiers".DIRECTORY_SEPARATOR.$fileName;
+		$copied = move_uploaded_file($_FILES['nomFichier']), $fullPathName;
+	}
+
+/*
+	if($copied) {
+		try{
+			$query = "INSERT INTO depot(id_projet, id_admin, chemin) VALUES (:pId, :pNum, :pFilePath)";
+			$stmt = $bdd->prepare ($query);
+			$stmt->bindParam(':pId', $id_projet);
+			$stmt->bindParam(':pNum', $id_admin);
+			$stmt->bindParam(':pFilePath', $chemin);
+			$nbInsert = $stmt->execute();
+		}
+		catch (Exception $e) {
+			$resultat = 0;
+		}
+
+	}
+	else{
+		$resultat = -4;
+	}
+*/
 ?>
+
+
+
+
+
+
+
+
+

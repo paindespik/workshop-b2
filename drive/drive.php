c8208ec94558d92460aea95d0f11091bad365561
paindespik <seandriard3@gmail.com>
2019-09-10 09:22:20 +0200

    new commit

---
+++ drive/drive.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Dépot de fichier</title>
+</head>
+
+<body>
+
+<h1>PROJETS HUB</h1>
+<p><a href="addproject.php">Créer projets<a></p>
+<p>ici: mes projets</p>
+
+<!-- envoie de la photo au fichier d'enregistrement -->
+<form action="addFile.php" method="post" enctype="multipart/form-data" class="form-inline">
+	<div class="form-group">
+		<label for="nomFichier">choisissez le fichier : </label>label>
+		<input type="file" class="form-control" name="nomFichier" id="nomFichier" placeholder="Entrer le fichier" value=""/>
+	</div>
+	<input type="hidden" name="file_id" id="file_id" value="<?php echo $photo_id ?>" />
+	<input type="submit" value="Ajouter" class="btn btn-default" />
+</form>
+
+</body>    

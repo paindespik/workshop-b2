582719d106f5d5003960dea8c1e44fff862ed2c0
Royo Arnaud <arnaud.royo@epsi.fr>
2019-09-12 14:36:23 +0200

    css projet fixed

--- drive/drive.php
+++ drive/drive.php
@@ -5,6 +5,33 @@
 <title>Votre projet</title>
 </head>
 
+<style>
+
+body{
+            background-color: #F76A19;
+            text-align: center;
+        }
+        h2{
+            max-width: 450px;
+  background: #FAFAFA;
+  padding: 30px;
+  margin: 50px auto;
+  box-shadow: 1px 1px 25px rgba(0, 0, 0, 0.35);
+  border-radius: 10px;
+  border: 6px solid #305A72;
+        }
+        h1{
+               max-width: 450px;
+  background: #FAFAFA;
+  padding: 30px;
+  margin: 50px auto;
+  box-shadow: 1px 1px 25px rgba(0, 0, 0, 0.35);
+  border-radius: 10px;
+  border: 6px solid #305A72;
+        } 
+        </style>
+
+
 <body> 
     <a href="../index.php">Retour</a>
 <?php 
